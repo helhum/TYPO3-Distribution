36f71e8742d7915d799eacf77519498c41cd85ea
Helmut Hummel <info@helhum.io>
2016-05-01 02:14:16 +0200

    Adapt to new config loader API

--- web/typo3conf/AdditionalConfiguration.php
+++ web/typo3conf/AdditionalConfiguration.php
@@ -1,10 +1,14 @@
 <?php
 // We let the loader load context and environment specific configuration
 // No other code must go in here!
-(new \Helhum\ConfigLoader\ConfigurationLoader(
-    $GLOBALS['TYPO3_CONF_VARS'],
-    \TYPO3\CMS\Core\Utility\GeneralUtility::getApplicationContext(),
-    dirname(dirname(__DIR__)) . '/conf',
-    'TYPO3',
-    '__'
-))->load();
+$context = \TYPO3\CMS\Core\Utility\GeneralUtility::getApplicationContext()->isProduction() ? 'production' : 'development';
+$confDir = dirname(dirname(__DIR__)) . '/conf';
+$configLoader = new \Helhum\ConfigLoader\ConfigurationLoader(
+    array(
+        new \Helhum\ConfigLoader\Reader\PhpFileReader($confDir . '/default.php'),
+        new \Helhum\ConfigLoader\Reader\PhpFileReader($confDir . '/' . $context . '.php'),
+        new \Helhum\ConfigLoader\Reader\EnvironmentReader('TYPO3'),
+        new \Helhum\ConfigLoader\Reader\PhpFileReader($confDir . '/override.php'),
+    )
+);
+$GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive($GLOBALS['TYPO3_CONF_VARS'], $configLoader->load());
