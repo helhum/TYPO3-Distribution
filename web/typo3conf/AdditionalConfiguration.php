ad24daeeeeef51568b1c30333466494bdc35dcf4
Helmut Hummel <info@helhum.io>
2016-05-01 04:52:28 +0200

    Use cached config loader
    remove .env caching now

--- web/typo3conf/AdditionalConfiguration.php
+++ web/typo3conf/AdditionalConfiguration.php
@@ -2,13 +2,20 @@
 // We let the loader load context and environment specific configuration
 // No other code must go in here!
 $context = \TYPO3\CMS\Core\Utility\GeneralUtility::getApplicationContext()->isProduction() ? 'production' : 'development';
-$confDir = dirname(dirname(__DIR__)) . '/conf';
-$configLoader = new \Helhum\ConfigLoader\ConfigurationLoader(
-    array(
-        new \Helhum\ConfigLoader\Reader\PhpFileReader($confDir . '/default.php'),
-        new \Helhum\ConfigLoader\Reader\PhpFileReader($confDir . '/' . $context . '.php'),
-        new \Helhum\ConfigLoader\Reader\EnvironmentReader('TYPO3'),
-        new \Helhum\ConfigLoader\Reader\PhpFileReader($confDir . '/override.php'),
-    )
-);
-$GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive($GLOBALS['TYPO3_CONF_VARS'], $configLoader->load());
+$rootDir = dirname(dirname(__DIR__));
+$confDir = $rootDir . '/conf';
+$cacheDir = $rootDir . '/var/cache';
+$cacheIdentifier = file_exists($rootDir . '/.env') ? md5($context . filemtime($rootDir . '/.env') . filemtime($rootDir . '/web/typo3conf/LocalConfiguration.php')) :  null;
+$GLOBALS['TYPO3_CONF_VARS'] = (new \Helhum\ConfigLoader\CachedConfigurationLoader(
+    $GLOBALS['TYPO3_CONF_VARS'],
+    $cacheDir,
+    $cacheIdentifier,
+    function() use ($confDir, $context) {
+        return new \Helhum\ConfigLoader\ConfigurationLoader(array(
+                new \Helhum\ConfigLoader\Reader\PhpFileReader($confDir . '/default.php'),
+                new \Helhum\ConfigLoader\Reader\PhpFileReader($confDir . '/' . $context . '.php'),
+                new \Helhum\ConfigLoader\Reader\EnvironmentReader('TYPO3'),
+                new \Helhum\ConfigLoader\Reader\PhpFileReader($confDir . '/override.php'),
+            ));
+    }
+))->load();
