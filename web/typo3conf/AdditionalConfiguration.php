07e53156d71078197a9795ba3a7a33be99e61742
Helmut Hummel <info@helhum.io>
2016-05-03 16:15:35 +0200

    Introduce config loader factory

--- web/typo3conf/AdditionalConfiguration.php
+++ web/typo3conf/AdditionalConfiguration.php
@@ -1,30 +1,12 @@
 <?php
 // We let the loader load context and environment specific configuration
 // No other code must go in here!
-$context = \TYPO3\CMS\Core\Utility\GeneralUtility::getApplicationContext()->isProduction() ? 'production' : 'development';
-$rootDir = dirname(dirname(__DIR__));
-$confDir = $rootDir . '/conf';
-$cacheDir = $rootDir . '/var/cache';
-if (getenv('CONFIGURATION_CACHE_IDENTIFIER')) {
-    // Freeze configuration with fixed identifier if requested
-    $cacheIdentifier = getenv('CONFIGURATION_CACHE_IDENTIFIER');
-} else {
-    $cacheIdentifier = file_exists($rootDir . '/.env') ? md5($context . filemtime($rootDir . '/.env') . filemtime($rootDir . '/web/typo3conf/LocalConfiguration.php')) :  null;
-}
+$configLoader = \Helhum\TYPO3\Distribution\ConfigLoaderFactory::buildLoader(
+    $context = \TYPO3\CMS\Core\Utility\GeneralUtility::getApplicationContext()->isProduction() ? 'production' : 'development',
+    $rootDir = dirname(dirname(__DIR__)),
+    $fixedCacheIdentifier = getenv('CONFIGURATION_CACHE_IDENTIFIER')
+);
 $GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive(
     $GLOBALS['TYPO3_CONF_VARS'],
-    (new \Helhum\ConfigLoader\CachedConfigurationLoader(
-        $cacheDir,
-        $cacheIdentifier,
-        function() use ($confDir, $context) {
-            return new \Helhum\ConfigLoader\ConfigurationLoader(
-                array(
-                    new \Helhum\ConfigLoader\Reader\PhpFileReader($confDir . '/default.php'),
-                    new \Helhum\ConfigLoader\Reader\PhpFileReader($confDir . '/' . $context . '.php'),
-                    new \Helhum\ConfigLoader\Reader\EnvironmentReader('TYPO3'),
-                    new \Helhum\ConfigLoader\Reader\PhpFileReader($confDir . '/override.php'),
-                )
-            );
-        }
-    ))->load()
+    $configLoader->load()
 );
