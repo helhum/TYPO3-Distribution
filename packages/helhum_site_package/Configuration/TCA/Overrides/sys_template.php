a6f70b8f6f087e5b2f6795648004f07f699ed07e
Helmut Hummel <info@helhum.io>
2019-11-03 13:59:39 +0100

    Add basic TypoScript to demo site package

---
+++ packages/helhum_site_package/Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,5 @@
+<?php
+
+(static function () {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('helhum_site_package', 'Configuration/TypoScript', 'Helhum\'s Site Package');
+})();
