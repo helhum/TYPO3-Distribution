ce06f3dfc5f5020f22b468172acad19b2f0170ca
Helmut Hummel <info@helhum.io>
2019-11-03 14:04:53 +0100

    Add commented errors to be able to demonstrate logging behaviour

---
+++ packages/helhum_site_package/ext_localconf.php
@@ -0,0 +1,13 @@
+<?php
+
+(static function () {
+//    $logger = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Log\LogManager::class)->getLogger('Helhum');
+//    $logger->warning(
+//    'warning Message',
+//        [
+//            'foo' => 'bar',
+//        ]
+//    );
+//
+//    throw new \Exception('sadf', 1572786055);
+})();
